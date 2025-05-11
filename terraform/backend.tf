aab4348ab4e00399873b15ada5b39a5cb95cd5ae
Subhash Peram <subhashperam@gmail.com>
2025-05-11 13:48:09 +0530

    Modified

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-tf-statet"
     key            = "fargate/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks"
