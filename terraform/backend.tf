8a9a99a186b39cc69b34e55bebfcea79a54e940a
Subhash Peram <subhashperam@gmail.com>
2025-05-11 16:03:33 +0530

    Updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "hackathon-terraform-state"
     key            = "fargate/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
 }
