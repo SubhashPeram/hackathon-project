9e9afbf0d7fe2665a45100764309616eddc2b963
Subhash Peram <subhashperam@gmail.com>
2025-05-11 14:17:09 +0530

    Modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,34 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.terraform_state.bucket
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "hackathon-cluster"
 }
